9253f1800f4aaa839b66f2ad3b647dcfc010ac65
kumok <bhh@byu.edu>
2014-12-28 21:10:08 -0700

    Initial Commit

---
+++ FizzBuzz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FizzBuzz
+//
+//  Created by Duc Ho on 12/28/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FizzBuzz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FizzBuzz
+//
+//  Created by Duc Ho on 12/28/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
